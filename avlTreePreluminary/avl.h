59e56991aea0def4a840f9e6e5c3f9be13d9e39d
Martin F <36092473+MartinTF@users.noreply.github.com>
2018-09-20 23:39:29 -0400

    Update avl.h

--- avlTreePreluminary/avl.h
+++ avlTreePreluminary/avl.h
@@ -1,7 +1,7 @@
-:x
+struct avlNode{
 	int balance ;/*keep in mind -1 ~ left 0~balanced 1~right */
 	int val;
-	struct avlNode *l;
+	struct avlNode *l; /* create left and right pointers*/ 
 	struct avlNode *r; 
 };
 typedef struct avlNode avlNode;
@@ -16,18 +16,19 @@ struct qNode{
 typedef struct qNode qNode;
 
 
-
+/*declare que functions*/
 int qEnqueue(qNode **root,avlNode *val);
 int qDequeue(qNode **node,int *out);
-int add_bst(avlNode **root,int val);
 int ProcessAvlLevel(qNode **que);
+
+/*declare avl functions*/
+int add_bst(avlNode **root,int val);
 int printAvlInOrder(avlNode *root);
 int printAvlLevelOrder(avlNode *root);
 int isAVL(avlNode **root);
 int ProccessInOrder(avlNode *root,int *outVal);
 int compare(avlNode *root);
 int compareDepth(avlNode *root);
-
 int depthCalc(avlNode *root,int currentDepth,int *maxDepth);
 int rotate(avlNode **root,unsigned int Left0_Right1);
 int dblrotate(avlNode **root,unsigned int MajLMinR0_MajRMinL1);
